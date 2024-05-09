60e6e9f3a6521a3ae51196831cc988ee2c23a6f5
Сергей <s3rg0sh4@gmail.com>
2024-05-09 08:09:12 +0300

    add docs

--- Combo/Logging/ServiceCollectionExtensions.cs
+++ Combo/Logging/ServiceCollectionExtensions.cs
@@ -4,6 +4,11 @@ using Serilog;
 
 public static class ServiceCollectionExtensions
 {
+	/// <summary>
+	/// Конфигурация логгирования
+	/// </summary>
+	/// <param name="builder"></param>
+	/// <param name="logger">Логгер, который используется до сборки приложения</param>
 	public static void ConfigureLogging(this WebApplicationBuilder builder, out Serilog.Core.Logger logger)
 	{
 		logger = new LoggerConfiguration()
