e2ae478108472101ff4681fb07c09217a831999d
Сергей <s3rg0sh4@gmail.com>
2024-05-18 09:18:26 +0300

    add filter tests

---
+++ Combo.Tests/Filters/NullIsBadRequestFilterTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Combo.Tests.Filters;
+using Combo.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+public class NullIsBadRequestFilterTests
+{
+	[Fact]
+	public void NullIsBadRequestFilter_NullResult()
+	{
+		// Arrange
+		var actionContext = new ActionExecutedContext(new(new DefaultHttpContext(), new(), new()), [], null)
+		{
+			Result = new OkObjectResult(null)
+		};
+
+		var filter = new NullIsBadRequestAttribute("Test");
+
+		// Act
+		filter.OnActionExecuted(actionContext);
+
+		// Assert
+		Assert.IsType<BadRequestObjectResult>(actionContext.Result);
+	}
+
+	[Fact]
+	public void NullIsBadRequestFilter_HasResult()
+	{
+		// Arrange
+		var actionContext = new ActionExecutedContext(new(new DefaultHttpContext(), new(), new()), [], null)
+		{
+			Result = new OkObjectResult(new())
+		};
+
+		var filter = new NullIsBadRequestAttribute("Test");
+
+		// Act
+		filter.OnActionExecuted(actionContext);
+
+		// Assert
+		Assert.IsType<OkObjectResult>(actionContext.Result);
+	}
+
+}
---
+++ Combo.Tests/Filters/NullIsNotFoundFilterTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Combo.Tests.Filters;
+using Combo.Filters;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class NullIsNotFoundFilterTests
+{
+	[Fact]
+	public void NullIsNotFoundFilter_NullResult()
+	{
+		// Arrange
+		var actionContext = new ActionExecutedContext(new(new DefaultHttpContext(), new(), new()), [], null)
+		{
+			Result = new OkObjectResult(null)
+		};
+
+		var filter = new NullIsNotFoundAttribute("Test");
+
+		// Act
+		filter.OnActionExecuted(actionContext);
+
+		// Assert
+		Assert.IsType<NotFoundObjectResult>(actionContext.Result);
+	}
+
+	[Fact]
+	public void NullIsNotFoundFilter_HasResult()
+	{
+		// Arrange
+		var actionContext = new ActionExecutedContext(new(new DefaultHttpContext(), new(), new()), [], null)
+		{
+			Result = new OkObjectResult(new())
+		};
+
+		var filter = new NullIsNotFoundAttribute("Test");
+
+		// Act
+		filter.OnActionExecuted(actionContext);
+
+		// Assert
+		Assert.IsType<OkObjectResult>(actionContext.Result);
+	}
+}
